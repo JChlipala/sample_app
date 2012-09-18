ade53b4cac8c944dcb0fcd4499ce1887f8adc9fc
Jason Chlipala <jason.chlipala@gmail.com>
2012-09-18 11:30:34 -0400

    Finish the signup form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+
   attr_accessible :name, :email, :password, :password_confirmation
   has_secure_password
 
@@ -9,6 +10,6 @@ class User < ActiveRecord::Base
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, 
   					uniqueness: { case_sensitive: false }
 
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, length: { minimum: 6 }
   validates :password_confirmation, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
-SampleApp::Application.routes.draw do
-  
-  match "/signup", to: 'users#new'
+SampleApp::Application.routes.draw do  
+  resources :users
 
   root :to => 'static_pages#home'
+  
+  match "/signup", to: 'users#new'
   match "/help", to: 'static_pages#help'
   match "/contact", to: 'static_pages#contact'
   match "/about", to: 'static_pages#about'
