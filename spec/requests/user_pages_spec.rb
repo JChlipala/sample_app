d0b704c797f3bd33752b2176ba3585a615219d79
Jason Chlipala <jason.chlipala@gmail.com>
2012-10-10 16:57:55 -0400

    Add user microposts

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -16,23 +16,6 @@ describe "User pages" do
     	it { should have_selector('h1',    text: 'All users') }
 	end
 
-	describe "delete links" do
-		it { should_not have_link('delete') }
-
-   	describe "as an admin user" do
-        	let(:admin) { FactoryGirl.create(:admin) }
-        	before do
-       		sign_in admin
-       		visit users_path
-        	end
-
-        	it { should have_link('delete', href: user_path(User.first)) }
-        	it "should be able to delete another user" do
-       		expect { click_link('delete') }.to change(User, :count).by(-1)
-        	end
-        	it { should_not have_link('delete', href: user_path(admin)) }
-      end
-	end
 
 	describe "signup page" do
 		before { visit signup_path }
